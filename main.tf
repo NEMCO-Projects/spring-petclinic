b4d58df394e9c9f3a820a287915bd94f139756f9
Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>
2025-04-29 13:10:54 +0530

    Update main.tf
    Signed-off-by: Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -14,3 +14,17 @@ resource "aws_instance" "tomcat_server" {
 output "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
 }
+
+resource "aws_instance" "mysql_server" {
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  security_groups = ["sgalltraffic"]  # Reference the existing security group
+  key_name      = "mujahed"
+  tags = {
+    Name = "mysql_server"
+  }
+}
+
+output "mysql_server_ip" {
+  value = aws_instance.mysql_server.public_ip
+}
